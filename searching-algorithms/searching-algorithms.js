04428b59f89ce28ca907924eb37b7ee0f203b85e
Thamarai-IND <thamarai.vivid@gmail.com>
2026-02-23 19:55:07 +0530

    searching algorithm entry file

---
+++ searching-algorithms/searching-algorithms.js
@@ -0,0 +1,8 @@
+
+
+/**
+ * 
+     Ternary Search is a divide-and-conquer search algorithm that works on sorted arrays. It divides the array into three parts and compares the target value with the two middle elements to determine which part of the array to search next. The optimized version of Ternary Search can be implemented by using a more efficient method for calculating the middle indices, such as using a logarithmic approach to estimate the position based on the distribution of the values in the array. This can further reduce the number of comparisons needed to find the target value.
+ * 
+ * 
+ * */
