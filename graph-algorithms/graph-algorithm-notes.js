7424158b037191d96dafb7bc6465317d5c6800f3
Thamarai-IND <thamarai.vivid@gmail.com>
2026-02-25 01:07:39 +0530

    list of algorithms

---
+++ graph-algorithms/graph-algorithm-notes.js
@@ -0,0 +1,69 @@
+
+/**
+ * 
+ * list of graph algorithms and their descriptions, advantages, disadvantages, use cases, and step-by-step explanations of how they work, along with their time and space complexities.
+ * 
+ * graph algorithms:
+ * - Depth-First Search (DFS)
+ * - Breadth-First Search (BFS)
+ * - Dijkstra's Algorithm
+ * - Bellman-Ford Algorithm
+ * - Floyd-Warshall Algorithm
+ * - Prim's Algorithm
+ * - Kruskal's Algorithm
+ * - Tarjan's Algorithm
+ * - Edmonds-Karp Algorithm
+ * - Cycle Detection Algorithm
+ * - Topological Sorting Algorithm
+ * - Strongly Connected Components Algorithm
+ * - Activity Selection Algorithm
+ * - Knapsack Problem Algorithm
+ * - Traveling Salesman Problem Algorithm
+ * - Graph Coloring Algorithm
+ * - Minimum Spanning Tree Algorithm
+ * - Maximum Flow Algorithm
+ * - Shortest Path Algorithm
+ * - Graph Traversal Algorithm
+ * - Graph Search Algorithm
+ * - Graph Partitioning Algorithm
+ * - Graph Clustering Algorithm
+ * - Graph Isomorphism Algorithm
+ * - Graph Matching Algorithm
+ * - Graph Embedding Algorithm
+ * - Graph Compression Algorithm
+ * - Graph Visualization Algorithm
+ * - Graph Neural Networks Algorithm
+ * - Graph Generative Models Algorithm
+ * - Graph Convolutional Networks Algorithm
+ * - Graph Attention Networks Algorithm
+ * - Graph Recurrent Neural Networks Algorithm
+ * - Graph Autoencoders Algorithm
+ * - Graph Generative Adversarial Networks Algorithm
+ * - Graph Reinforcement Learning Algorithm
+ * - Graph Representation Learning Algorithm
+ * - Graph Anomaly Detection Algorithm
+ * - Graph Community Detection Algorithm
+ * - Graph Link Prediction Algorithm
+ * - Graph Node Classification Algorithm
+ * - Graph Edge Classification Algorithm
+ * - Graph Clustering Coefficient Algorithm
+ * - Graph Centrality Measures Algorithm
+ * - Graph PageRank Algorithm
+ * - Graph HITS Algorithm
+ * - Graph Betweenness Centrality Algorithm
+ * - Graph Closeness Centrality Algorithm
+ * - Graph Eigenvector Centrality Algorithm
+ * - Graph Degree Centrality Algorithm
+ * - Graph K-Core Decomposition Algorithm
+ * - Graph Louvain Method Algorithm
+ * - Graph Label Propagation Algorithm
+ * - Graph Infomap Algorithm
+ * - Graph Girvan-Newman Algorithm
+ * - Graph Walktrap Algorithm
+ * - Graph Leiden Algorithm
+ * - Graph Spectral Clustering Algorithm
+ * - Graph Modularity Optimization Algorithm
+ * 
+ * 
+ * 
+ */
